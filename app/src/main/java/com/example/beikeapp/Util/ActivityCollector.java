b6ac0b381a95d6869e5192bad100c29c2ae6ddf7
TangZaiChuan <406857586@qq.com>
2018-03-30 19:26:52 +0800

    MobSDK

---
+++ app/src/main/java/com/example/beikeapp/Util/ActivityCollector.java
@@ -0,0 +1,35 @@
+package com.example.beikeapp.Util;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by SugarSugar on 2018/3/21.
+ */
+
+/*
+    活动管理
+ */
+public class ActivityCollector extends AppCompatActivity {
+
+    public static List<Activity> activities = new ArrayList<>();
+
+    public static void addActivity(Activity activity){
+        activities.add(activity);
+    }
+
+    public static void removeActivity(Activity activity){
+        activities.remove(activity);
+    }
+
+    public static void finishAll(){
+        for (Activity activity: activities){
+            if(!activity.isFinishing()){
+                activity.finish();
+            }
+        }
+    }
+}
